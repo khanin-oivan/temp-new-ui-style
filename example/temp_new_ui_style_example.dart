e080a0aaddfe0e079b77cef2db47fbb72167248b
khanin-oivan <nin.khanin@oivan.com>
2024-08-16 11:12:35 +0700

    init dart package

---
+++ example/temp_new_ui_style_example.dart
@@ -0,0 +1,6 @@
+import 'package:temp_new_ui_style/temp_new_ui_style.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/temp_new_ui_style.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/temp_new_ui_style_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
