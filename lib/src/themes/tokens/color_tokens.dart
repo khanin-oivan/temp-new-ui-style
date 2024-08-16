960677cc42e95aa7a51805d47314f3560ab7d1bb
khanin-oivan <nin.khanin@oivan.com>
2024-08-16 13:46:47 +0700

    add color tokens

---
+++ lib/src/themes/tokens/color_tokens.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+
+// COLORS
+// -- Primary
+const Color colorPrimary10 = Color(0xff1F3102);
+const Color colorPrimary20 = Color(0xff1F3102);
+const Color colorPrimary30 = Color(0xff1F3102);
+const Color colorPrimary40 = Color(0xff1F3102);
+const Color colorPrimary50 = Color(0xff1F3102);
+const Color colorPrimary60 = Color(0xff1F3102);
+const Color colorPrimary70 = Color(0xff1F3102);
+const Color colorPrimary80 = Color(0xff1F3102);
+const Color colorPrimary90 = Color(0xff1F3102);
+const Color colorPrimary95 = Color(0xff1F3102);
+const Color colorPrimary99 = Color(0xff1F3102);
+
+// -- Secondary
+const Color colorSecondary10 = Color(0xff1F3102);
+const Color colorSecondary20 = Color(0xff1F3102);
+const Color colorSecondary30 = Color(0xff1F3102);
+const Color colorSecondary40 = Color(0xff1F3102);
+const Color colorSecondary50 = Color(0xff1F3102);
+const Color colorSecondary60 = Color(0xff1F3102);
+const Color colorSecondary70 = Color(0xff1F3102);
+const Color colorSecondary80 = Color(0xff1F3102);
+const Color colorSecondary90 = Color(0xff1F3102);
+const Color colorSecondary95 = Color(0xff1F3102);
+const Color colorSecondary99 = Color(0xff1F3102);
+
+// -- Success
+const Color colorSuccess10 = Color(0xff1F3102);
+const Color colorSuccess20 = Color(0xff1F3102);
+const Color colorSuccess30 = Color(0xff1F3102);
+const Color colorSuccess40 = Color(0xff1F3102);
+const Color colorSuccess50 = Color(0xff1F3102);
+const Color colorSuccess60 = Color(0xff1F3102);
+const Color colorSuccess70 = Color(0xff1F3102);
+const Color colorSuccess80 = Color(0xff1F3102);
+const Color colorSuccess90 = Color(0xff1F3102);
+const Color colorSuccess95 = Color(0xff1F3102);
+const Color colorSuccess99 = Color(0xff1F3102);
+
+// -- Warning
+const Color colorWarning10 = Color(0xff1F3102);
+const Color colorWarning20 = Color(0xff1F3102);
+const Color colorWarning30 = Color(0xff1F3102);
+const Color colorWarning40 = Color(0xff1F3102);
+const Color colorWarning50 = Color(0xff1F3102);
+const Color colorWarning60 = Color(0xff1F3102);
+const Color colorWarning70 = Color(0xff1F3102);
+const Color colorWarning80 = Color(0xff1F3102);
+const Color colorWarning90 = Color(0xff1F3102);
+const Color colorWarning95 = Color(0xff1F3102);
+const Color colorWarning99 = Color(0xff1F3102);
+
+// -- Danger
+const Color colorDanger10 = Color(0xff1F3102);
+const Color colorDanger20 = Color(0xff1F3102);
+const Color colorDanger30 = Color(0xff1F3102);
+const Color colorDanger40 = Color(0xff1F3102);
+const Color colorDanger50 = Color(0xff1F3102);
+const Color colorDanger60 = Color(0xff1F3102);
+const Color colorDanger70 = Color(0xff1F3102);
+const Color colorDanger80 = Color(0xff1F3102);
+const Color colorDanger90 = Color(0xff1F3102);
+const Color colorDanger95 = Color(0xff1F3102);
+const Color colorDanger99 = Color(0xff1F3102);
+
+// -- AIS Customer
+const Color colorAisCustomer10 = Color(0xff1F3102);
+const Color colorAisCustomer20 = Color(0xff1F3102);
+const Color colorAisCustomer30 = Color(0xff1F3102);
+const Color colorAisCustomer40 = Color(0xff1F3102);
+const Color colorAisCustomer50 = Color(0xff1F3102);
+const Color colorAisCustomer60 = Color(0xff1F3102);
+const Color colorAisCustomer70 = Color(0xff1F3102);
+const Color colorAisCustomer80 = Color(0xff1F3102);
+const Color colorAisCustomer90 = Color(0xff1F3102);
+const Color colorAisCustomer95 = Color(0xff1F3102);
+const Color colorAisCustomer99 = Color(0xff1F3102);
+
+// -- Emerald
+const Color colorEmerald10 = Color(0xff1F3102);
+const Color colorEmerald20 = Color(0xff1F3102);
+const Color colorEmerald30 = Color(0xff1F3102);
+const Color colorEmerald40 = Color(0xff1F3102);
+const Color colorEmerald50 = Color(0xff1F3102);
+const Color colorEmerald60 = Color(0xff1F3102);
+const Color colorEmerald70 = Color(0xff1F3102);
+const Color colorEmerald80 = Color(0xff1F3102);
+const Color colorEmerald90 = Color(0xff1F3102);
+const Color colorEmerald95 = Color(0xff1F3102);
+const Color colorEmerald99 = Color(0xff1F3102);
+
+// -- Gold
+const Color colorGold10 = Color(0xff1F3102);
+const Color colorGold20 = Color(0xff1F3102);
+const Color colorGold30 = Color(0xff1F3102);
+const Color colorGold40 = Color(0xff1F3102);
+const Color colorGold50 = Color(0xff1F3102);
+const Color colorGold60 = Color(0xff1F3102);
+const Color colorGold70 = Color(0xff1F3102);
+const Color colorGold80 = Color(0xff1F3102);
+const Color colorGold90 = Color(0xff1F3102);
+const Color colorGold95 = Color(0xff1F3102);
+const Color colorGold99 = Color(0xff1F3102);
+
+// -- Platinum
+const Color colorPlatinum10 = Color(0xff1F3102);
+const Color colorPlatinum20 = Color(0xff1F3102);
+const Color colorPlatinum30 = Color(0xff1F3102);
+const Color colorPlatinum40 = Color(0xff1F3102);
+const Color colorPlatinum50 = Color(0xff1F3102);
+const Color colorPlatinum60 = Color(0xff1F3102);
+const Color colorPlatinum70 = Color(0xff1F3102);
+const Color colorPlatinum80 = Color(0xff1F3102);
+const Color colorPlatinum90 = Color(0xff1F3102);
+const Color colorPlatinum95 = Color(0xff1F3102);
+const Color colorPlatinum99 = Color(0xff1F3102);
+
+// -- Privilege
+const Color colorPrivilege10 = Color(0xff1F3102);
+const Color colorPrivilege20 = Color(0xff1F3102);
+const Color colorPrivilege30 = Color(0xff1F3102);
+const Color colorPrivilege40 = Color(0xff1F3102);
+const Color colorPrivilege50 = Color(0xff1F3102);
+const Color colorPrivilege60 = Color(0xff1F3102);
+const Color colorPrivilege70 = Color(0xff1F3102);
+const Color colorPrivilege80 = Color(0xff1F3102);
+const Color colorPrivilege90 = Color(0xff1F3102);
+const Color colorPrivilege95 = Color(0xff1F3102);
+const Color colorPrivilege99 = Color(0xff1F3102);
+
+// -- Points
+const Color colorPoints10 = Color(0xff1F3102);
+const Color colorPoints20 = Color(0xff1F3102);
+const Color colorPoints30 = Color(0xff1F3102);
+const Color colorPoints40 = Color(0xff1F3102);
+const Color colorPoints50 = Color(0xff1F3102);
+const Color colorPoints60 = Color(0xff1F3102);
+const Color colorPoints70 = Color(0xff1F3102);
+const Color colorPoints80 = Color(0xff1F3102);
+const Color colorPoints90 = Color(0xff1F3102);
+const Color colorPoints95 = Color(0xff1F3102);
+const Color colorPoints99 = Color(0xff1F3102);
+
+// -- Neutral
+const Color colorNeutral10 = Color(0xff1F3102);
+const Color colorNeutral20 = Color(0xff1F3102);
+const Color colorNeutral30 = Color(0xff1F3102);
+const Color colorNeutral40 = Color(0xff1F3102);
+const Color colorNeutral50 = Color(0xff1F3102);
+const Color colorNeutral60 = Color(0xff1F3102);
+const Color colorNeutral70 = Color(0xff1F3102);
+const Color colorNeutral80 = Color(0xff1F3102);
+const Color colorNeutral90 = Color(0xff1F3102);
+const Color colorNeutral95 = Color(0xff1F3102);
+const Color colorNeutral99 = Color(0xff1F3102);
+
+// -- Black
+const Color colorBlack5 = Color(0xff1F3102);
+const Color colorBlack10 = Color(0xff1F3102);
+const Color colorBlack20 = Color(0xff1F3102);
+const Color colorBlack30 = Color(0xff1F3102);
+const Color colorBlack40 = Color(0xff1F3102);
+const Color colorBlack50 = Color(0xff1F3102);
+const Color colorBlack60 = Color(0xff1F3102);
+const Color colorBlack70 = Color(0xff1F3102);
+const Color colorBlack80 = Color(0xff1F3102);
+const Color colorBlack90 = Color(0xff1F3102);
+const Color colorBlack100 = Color(0xff1F3102);
+
+// -- White
+const Color colorWhite5 = Color(0xff1F3102);
+const Color colorWhite10 = Color(0xff1F3102);
+const Color colorWhite20 = Color(0xff1F3102);
+const Color colorWhite30 = Color(0xff1F3102);
+const Color colorWhite40 = Color(0xff1F3102);
+const Color colorWhite50 = Color(0xff1F3102);
+const Color colorWhite60 = Color(0xff1F3102);
+const Color colorWhite70 = Color(0xff1F3102);
+const Color colorWhite80 = Color(0xff1F3102);
+const Color colorWhite90 = Color(0xff1F3102);
+const Color colorWhite100 = Color(0xff1F3102);
+
+// -- Store
+const Color colorStore10 = Color(0xff1F3102);
+const Color colorStore20 = Color(0xff1F3102);
+const Color colorStore30 = Color(0xff1F3102);
+const Color colorStore40 = Color(0xff1F3102);
+const Color colorStore50 = Color(0xff1F3102);
+const Color colorStore60 = Color(0xff1F3102);
+const Color colorStore70 = Color(0xff1F3102);
+const Color colorStore80 = Color(0xff1F3102);
+const Color colorStore90 = Color(0xff1F3102);
+const Color colorStore95 = Color(0xff1F3102);
+const Color colorStore99 = Color(0xff1F3102);
+
+// -- Entertainment
+const Color colorEntertainment10 = Color(0xff1F3102);
+const Color colorEntertainment20 = Color(0xff1F3102);
+const Color colorEntertainment30 = Color(0xff1F3102);
+const Color colorEntertainment40 = Color(0xff1F3102);
+const Color colorEntertainment50 = Color(0xff1F3102);
+const Color colorEntertainment60 = Color(0xff1F3102);
+const Color colorEntertainment70 = Color(0xff1F3102);
+const Color colorEntertainment80 = Color(0xff1F3102);
+const Color colorEntertainment90 = Color(0xff1F3102);
+const Color colorEntertainment95 = Color(0xff1F3102);
+const Color colorEntertainment99 = Color(0xff1F3102);
+
+// -- Star
+const Color colorStar10 = Color(0xff1F3102);
+const Color colorStar20 = Color(0xff1F3102);
+const Color colorStar30 = Color(0xff1F3102);
+const Color colorStar40 = Color(0xff1F3102);
+const Color colorStar50 = Color(0xff1F3102);
+const Color colorStar60 = Color(0xff1F3102);
+const Color colorStar70 = Color(0xff1F3102);
+const Color colorStar80 = Color(0xff1F3102);
+const Color colorStar90 = Color(0xff1F3102);
+const Color colorStar95 = Color(0xff1F3102);
+const Color colorStar99 = Color(0xff1F3102);
+
+// -- Fibre
+const Color colorFibre10 = Color(0xff1F3102);
+const Color colorFibre20 = Color(0xff1F3102);
+const Color colorFibre30 = Color(0xff1F3102);
+const Color colorFibre40 = Color(0xff1F3102);
+const Color colorFibre50 = Color(0xff1F3102);
+const Color colorFibre60 = Color(0xff1F3102);
+const Color colorFibre70 = Color(0xff1F3102);
+const Color colorFibre80 = Color(0xff1F3102);
+const Color colorFibre90 = Color(0xff1F3102);
+const Color colorFibre95 = Color(0xff1F3102);
+const Color colorFibre99 = Color(0xff1F3102);
