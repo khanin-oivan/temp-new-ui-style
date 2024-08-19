07ab660411b2f2a73082a85db4e66afe2d1e80f7
khanin-oivan <nin.khanin@oivan.com>
2024-08-19 14:56:04 +0700

    add text style semantic tokens

---
+++ lib/src/themes/mya_text_style.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MyaTextStyle extends ThemeExtension<MyaTextStyle> {
+  MyaTextStyle({
+    required this.displayLarge,
+    required this.displayMedium,
+    required this.displaySmall,
+    required this.headlineLarge,
+    required this.headlineMedium,
+    required this.headlineSmall,
+    required this.titleLarge,
+    required this.titleMedium,
+    required this.titleSmall,
+    required this.bodyLarge,
+    required this.bodyMedium,
+    required this.bodySmall,
+    required this.labelLarge,
+    required this.labelMedium,
+    required this.labelSmall,
+  });
+
+  final TextStyle displayLarge;
+  final TextStyle displayMedium;
+  final TextStyle displaySmall;
+  final TextStyle headlineLarge;
+  final TextStyle headlineMedium;
+  final TextStyle headlineSmall;
+  final TextStyle titleLarge;
+  final TextStyle titleMedium;
+  final TextStyle titleSmall;
+  final TextStyle bodyLarge;
+  final TextStyle bodyMedium;
+  final TextStyle bodySmall;
+  final TextStyle labelLarge;
+  final TextStyle labelMedium;
+  final TextStyle labelSmall;
+
+  @override
+  MyaTextStyle copyWith() {
+    //TODO: if implement copy with, must change declaration to nullable.
+    throw UnimplementedError();
+  }
+
+  @override
+  MyaTextStyle lerp(MyaTextStyle? other, double t) {
+    //TODO
+    return this;
+  }
+}
