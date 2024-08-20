6d083f01eef93a21554b7e4f7cdf9ac7693e8ffa
khanin-oivan <nin.khanin@oivan.com>
2024-08-20 11:40:28 +0700

    add spacing

---
+++ lib/src/themes/spacing/mya_spacing.dart
@@ -0,0 +1,29 @@
+import 'package:temp_new_ui_style/src/themes/spacing/spacing_tokens.dart';
+
+// Margin
+const kVerticalMargin1 = kSpace7;
+const kHorizontalMargin1 = kSpace1;
+const kHorizontalMargin2 = kSpace3;
+
+// Padding
+const kPadding1 = kSpace1;
+const kPadding2 = kSpace2;
+const kPadding3 = kSpace3;
+const kPadding4 = kSpace4;
+const kPadding5 = kSpace5;
+const kPadding6 = kSpace6;
+const kPadding7 = kSpace7;
+const kPadding8 = kSpace8;
+const kPadding9 = kSpace9;
+const kPadding10 = kSpace11;
+
+//Gap
+const kGap1 = kSpace1;
+const kGap2 = kSpace2;
+const kGap3 = kSpace3;
+const kGap4 = kSpace4;
+const kGap5 = kSpace5;
+const kGap6 = kSpace6;
+const kGap7 = kSpace7;
+const kGap8 = kSpace9;
+const kGap9 = kSpace11;
