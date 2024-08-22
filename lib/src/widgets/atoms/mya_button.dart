dd0a4c63964bf9c3226246bd0610131511be9365
khanin-oivan <nin.khanin@oivan.com>
2024-08-22 11:14:11 +0700

    wip

--- lib/src/widgets/atoms/mya_button.dart
+++ lib/src/widgets/atoms/mya_button.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:temp_new_ui_style/src/themes/colors/mya_theme_colors.dart';
+import 'package:temp_new_ui_style/src/themes/text_styles/mya_text_style.dart';
 import 'package:temp_new_ui_style/src/utils/enum.dart';
+import 'package:temp_new_ui_style/temp_new_ui_style.dart';
 
 class ChmButtonCountdown extends StatefulWidget {
   const ChmButtonCountdown({
@@ -10,25 +13,20 @@ class ChmButtonCountdown extends StatefulWidget {
     required this.onPressed,
     this.style = MyaButtonStyle.filled,
     this.size = MyaButtonSize.large,
+    this.theme = MyaButtonTheme.primary,
     this.leadingIconKey,
     this.trailingIconKey,
-    this.cdSeconds = 0,
     this.width,
-    this.hideLabelWhenCount = false,
-    this.countWhenInit = true,
-  }) : assert(cdSeconds >= 0, 'cdSeconds must be greater than or equal to 0');
+  });
 
-  final MyaButtonTheme theme = MyaButtonTheme.primary;
+  final MyaButtonTheme theme;
   final MyaButtonStyle style;
   final MyaButtonSize size;
-  final Future<bool?> Function()? onPressed;
+  final Function()? onPressed;
   final String label;
   final String? leadingIconKey;
   final String? trailingIconKey;
-  final int cdSeconds;
   final double? width;
-  final bool hideLabelWhenCount;
-  final bool countWhenInit;
 
   @override
   State<ChmButtonCountdown> createState() => _ChmButtonCountdownState();
@@ -39,39 +37,14 @@ class _ChmButtonCountdownState extends State<ChmButtonCountdown> {
   late double iconSize;
   bool isDisable = false;
   bool isPressed = false;
-  bool isCounting = false;
-  int remainingSeconds = 0;
-  Timer? _timer;
 
   @override
   void dispose() {
-    _timer?.cancel();
     super.dispose();
-  }
-
-  void startCountdown() {
-    setState(() {
-      isDisable = true; //handle loading before countdown
-      isCounting = true;
-      remainingSeconds = widget.cdSeconds;
-    });
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        if (remainingSeconds > 0) {
-          remainingSeconds--;
-          if (remainingSeconds == 0) {
-            isCounting = false;
-            isDisable = false;
-            timer.cancel();
-          }
-        }
-      });
-    });
   }
 
   @override
   void initState() {
-    if (widget.cdSeconds > 0 && widget.countWhenInit) startCountdown();
     isLargeButton = widget.size == MyaButtonSize.large;
     iconSize = isLargeButton ? 24 : 16;
 
@@ -80,18 +53,19 @@ class _ChmButtonCountdownState extends State<ChmButtonCountdown> {
 
   @override
   Widget build(BuildContext context) {
-    final Set<MaterialState> states = isDisable
-        ? {MaterialState.disabled}
+    final MyaTextStyle myaTextStyle = context.myaTextStyle;
+    final MyaThemeColors myaColors = context.myaThemeColors;
+
+    final Set<WidgetState> states = isDisable
+        ? {WidgetState.disabled}
         : isPressed
-            ? {MaterialState.pressed}
+            ? {WidgetState.pressed}
             : {};
 
     final ButtonStyle buttonStyle =
         _getButtonStyle(widget.style, isLargeButton, widget.width);
 
     final Color? foregroundColor = buttonStyle.foregroundColor!.resolve(states);
-
-    bool shouldShowLabel = !(widget.hideLabelWhenCount && isCounting);
 
     return GestureDetector(
       //Need gesture detector because ChmIcon() does not inherit MaterialPropertyState, like Icon() in Flutter.
@@ -111,23 +85,7 @@ class _ChmButtonCountdownState extends State<ChmButtonCountdown> {
         });
       },
       child: ElevatedButton(
-        onPressed: isDisable
-            ? null
-            : () async {
-                setState(() {
-                  isDisable = true; //handle loading before countdown
-                });
-                bool? isSuccess = await widget.onPressed!();
-                isSuccess = isSuccess ?? false;
-
-                if (isSuccess == false && widget.cdSeconds > 0) {
-                  startCountdown();
-                } else {
-                  setState(() {
-                    isDisable = false; //handle loading before countdown
-                  });
-                }
-              },
+        onPressed: isDisable ? null : widget.onPressed,
         style: buttonStyle,
         child: Row(
           mainAxisSize: MainAxisSize.min,
@@ -135,47 +93,39 @@ class _ChmButtonCountdownState extends State<ChmButtonCountdown> {
             if (widget.leadingIconKey != null)
               Padding(
                 padding: EdgeInsets.only(left: isLargeButton ? 16 : 12),
-                child: ChmIcon(
-                  keyIconName: widget.leadingIconKey as String,
+                child: Icon(
+                  Icons.circle_rounded,
+                  size: iconSize,
                   color: foregroundColor,
-                  width: iconSize,
-                  height: iconSize,
                 ),
+                // child: ChmIcon(
+                //   keyIconName: widget.leadingIconKey as String,
+                //   color: foregroundColor,
+                //   width: iconSize,
+                //   height: iconSize,
+                // ),
               ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: isLargeButton ? 16 : 8),
-              child: Text.rich(
-                textAlign: TextAlign.center,
-                TextSpan(
-                  style: isLargeButton
-                      ? BaseTextStyle.bodyBold.copyWith(color: foregroundColor)
-                      : BaseTextStyle.bodyMediumBold
-                          .copyWith(color: foregroundColor),
-                  children: [
-                    if (shouldShowLabel) TextSpan(text: widget.label),
-                    if (shouldShowLabel || remainingSeconds > 0)
-                      const TextSpan(text: ' '),
-                    if (remainingSeconds > 0)
-                      shouldShowLabel
-                          ? TextSpan(
-                              text: '(${_formatDuration(remainingSeconds)})',
-                            )
-                          : TextSpan(
-                              text: _formatDuration(remainingSeconds),
-                            ),
-                  ],
-                ),
+              child: Text(
+                widget.label,
+                style: myaTextStyle.bodyLargeBold,
               ),
             ),
             if (widget.trailingIconKey != null)
               Padding(
                 padding: EdgeInsets.only(right: isLargeButton ? 16 : 12),
-                child: ChmIcon(
-                  keyIconName: widget.trailingIconKey as String,
+                child: Icon(
+                  Icons.circle_rounded,
+                  size: iconSize,
                   color: foregroundColor,
-                  width: iconSize,
-                  height: iconSize,
                 ),
+                // child: ChmIcon(
+                //   keyIconName: widget.trailingIconKey as String,
+                //   color: foregroundColor,
+                //   width: iconSize,
+                //   height: iconSize,
+                // ),
               ),
           ],
         ),
@@ -185,15 +135,15 @@ class _ChmButtonCountdownState extends State<ChmButtonCountdown> {
 }
 
 ButtonStyle _getButtonStyle(
-    StyleButton style, bool isLargeButton, double? width) {
+    MyaButtonStyle style, bool isLargeButton, double? width) {
   final commonButtonStyle = ButtonStyle(
-    padding: MaterialStateProperty.all(EdgeInsets.zero),
+    padding: WidgetStateProperty.all(EdgeInsets.zero),
     splashFactory: NoSplash.splashFactory,
-    overlayColor: MaterialStateProperty.all(Colors.transparent),
-    elevation: MaterialStateProperty.all(0),
+    overlayColor: WidgetStateProperty.all(Colors.transparent),
+    elevation: WidgetStateProperty.all(0),
     animationDuration: Duration.zero,
     minimumSize:
-        MaterialStateProperty.all(Size(width ?? 100, isLargeButton ? 48 : 32)),
+        WidgetStateProperty.all(Size(width ?? 100, isLargeButton ? 48 : 32)),
   );
 
   final commonShape = RoundedRectangleBorder(
@@ -201,52 +151,52 @@ ButtonStyle _getButtonStyle(
   );
 
   switch (style) {
-    case (StyleButton.filled):
+    case (MyaButtonStyle.filled):
       return commonButtonStyle.copyWith(
-        shape: MaterialStateProperty.all(commonShape),
-        foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+        shape: WidgetStateProperty.all(commonShape),
+        foregroundColor: WidgetStateProperty.resolveWith<Color?>(
           // Color of text and icon
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.disabled)) {
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.disabled)) {
               return BaseColors.textDisable;
             }
-            if (states.contains(MaterialState.pressed) ||
-                states.contains(MaterialState.hovered)) {
+            if (states.contains(WidgetState.pressed) ||
+                states.contains(WidgetState.hovered)) {
               return BaseColors.whiteColor;
             }
             return BaseColors
                 .allPrimaryPrimary10; // Defer to the widget's default.
           },
         ),
-        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.disabled)) {
+        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.disabled)) {
               return BaseColors.disableBackground;
             }
-            if (states.contains(MaterialState.hovered)) {
+            if (states.contains(WidgetState.hovered)) {
               return BaseColors.primaryHover;
             }
-            if (states.contains(MaterialState.pressed)) {
+            if (states.contains(WidgetState.pressed)) {
               return BaseColors.primaryPressed;
             }
             return BaseColors.primaryPrimary; // Defer to the widget's default.
           },
         ),
       );
-    case (StyleButton.outlined):
+    case (MyaButtonStyle.outlined):
       return commonButtonStyle.copyWith(
-        shape: MaterialStateProperty.resolveWith((states) {
-          if (states.contains(MaterialState.disabled)) {
+        shape: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.disabled)) {
             return commonShape.copyWith(
                 side:
                     const BorderSide(width: 1, color: BaseColors.textDisable));
           }
-          if (states.contains(MaterialState.pressed)) {
+          if (states.contains(WidgetState.pressed)) {
             return commonShape.copyWith(
                 side: const BorderSide(
                     width: 1, color: BaseColors.primaryPressed));
           }
-          if (states.contains(MaterialState.hovered)) {
+          if (states.contains(WidgetState.hovered)) {
             return commonShape.copyWith(
                 side:
                     const BorderSide(width: 1, color: BaseColors.primaryHover));
@@ -255,93 +205,93 @@ ButtonStyle _getButtonStyle(
               side: const BorderSide(
                   width: 1, color: BaseColors.allPrimaryPrimary10));
         }),
-        foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+        foregroundColor: WidgetStateProperty.resolveWith<Color?>(
           // Color of text and icon
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.disabled)) {
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.disabled)) {
               return BaseColors.textDisable;
             }
-            if (states.contains(MaterialState.pressed)) {
+            if (states.contains(WidgetState.pressed)) {
               return BaseColors.primaryPressed;
             }
 
-            if (states.contains(MaterialState.hovered)) {
+            if (states.contains(WidgetState.hovered)) {
               return BaseColors.primaryHover;
             }
             return BaseColors
                 .allPrimaryPrimary10; // Defer to the widget's default.
           },
         ),
-        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.pressed)) {
+        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.pressed)) {
               return BaseColors.surfacelight5;
             }
             return BaseColors.whiteColor; // Defer to the widget's default.
           },
         ),
       );
-    case (StyleButton.text):
+    case (MyaButtonStyle.text):
       return commonButtonStyle.copyWith(
-        shape: MaterialStateProperty.all(commonShape),
-        foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+        shape: WidgetStateProperty.all(commonShape),
+        foregroundColor: WidgetStateProperty.resolveWith<Color?>(
           // Color of text and icon
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.disabled)) {
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.disabled)) {
               return BaseColors.textDisable;
             }
-            if (states.contains(MaterialState.pressed)) {
+            if (states.contains(WidgetState.pressed)) {
               return BaseColors.primaryPressed;
             }
 
-            if (states.contains(MaterialState.hovered)) {
+            if (states.contains(WidgetState.hovered)) {
               return BaseColors.primaryHover;
             }
             return BaseColors
                 .allPrimaryPrimary10; // Defer to the widget's default.
           },
         ),
-        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-          (Set<MaterialState> states) {
+        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+          (Set<WidgetState> states) {
             return BaseColors.transparent; // Defer to the widget's default.
           },
         ),
       );
-    case (StyleButton.elevated):
+    case (MyaButtonStyle.elevated):
       return commonButtonStyle.copyWith(
-        elevation: MaterialStateProperty.resolveWith<double?>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.pressed)) {
+        elevation: WidgetStateProperty.resolveWith<double?>(
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.pressed)) {
               return 0;
             }
 
             return 2;
           },
         ),
-        shape: MaterialStateProperty.all(commonShape),
-        foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+        shape: WidgetStateProperty.all(commonShape),
+        foregroundColor: WidgetStateProperty.resolveWith<Color?>(
           // Color of text and icon
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.disabled)) {
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.disabled)) {
               return BaseColors.textDisable;
             }
-            if (states.contains(MaterialState.pressed) ||
-                states.contains(MaterialState.hovered)) {
+            if (states.contains(WidgetState.pressed) ||
+                states.contains(WidgetState.hovered)) {
               return BaseColors.whiteColor;
             }
             return BaseColors
                 .allPrimaryPrimary10; // Defer to the widget's default.
           },
         ),
-        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.disabled)) {
+        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+          (Set<WidgetState> states) {
+            if (states.contains(WidgetState.disabled)) {
               return BaseColors.disableBackground;
             }
-            if (states.contains(MaterialState.hovered)) {
+            if (states.contains(WidgetState.hovered)) {
               return BaseColors.primaryHover;
             }
-            if (states.contains(MaterialState.pressed)) {
+            if (states.contains(WidgetState.pressed)) {
               return BaseColors.primaryPressed;
             }
             return BaseColors.primaryPrimary; // Defer to the widget's default.
